3a9345db1c663fb9d0ca8e83a5d89463f7f9ac34
Armando Barrera <armandobarrera87@gmail.com>
2019-02-14 13:28:16 +0100

    added morgan and nodemon

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express = require("express");
+const logger = require("morgan");
+
 const app = express();
+
+// Check if the server is running on production or on local environment to send express header
+const dev = app.get("env") !== "production";
+if (!dev) {
+  app.disable("x-powered-by");
+  app.use(logger("common"));
+  //  app.use(compression());
+} else {
+  app.use(logger("dev"));
+}
 
 app.get("/", (req, res) => res.send("hello world"));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("REST API running on port 3000");
 });
